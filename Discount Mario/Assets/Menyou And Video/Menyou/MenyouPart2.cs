b50a93b61e945f7153a951e1226c7bc03ca4b9cc
charlieDearborn <33422420+charlieDearborn@users.noreply.github.com>
2018-06-17 08:54:14 -0300

    qwer
    qwre

---
+++ Discount Mario/Assets/Menyou And Video/Menyou/MenyouPart2.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenyouPart2 : MonoBehaviour
+{
+    public int index = 0;
+    public int toadleOpshons = 3;
+    public float Ofiset = 1f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (index < toadleOpshons - 1)
+            {
+                index++;
+                Vector2 position = transform.position;
+                position.y -= Ofiset;
+                transform.position = position;
+
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (index > 0)
+            {
+                index--;
+                Vector2 position = transform.position;
+                position.y += Ofiset;
+                transform.position = position;
+
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (index == 0)
+            {
+                SceneManager.LoadScene("Start");
+            }
+            if (index == 1)
+            {
+                SceneManager.LoadScene("Skins");
+                //or 
+
+            }
+            if (index == 2)
+            {
+                SceneManager.LoadScene("Opshons");
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Discount Mario/Assets/Menyou And Video/Video/VideoOBJ.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class VideoOBJ : MonoBehaviour {
+
+    public VideoClip[] videoClips;
+    private VideoPlayer videoPlayer;
+
+
+    // Use this for initialization
+    void Start () {
+
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.targetTexture.Release();
+        int randomClip = Random.Range(0, 4);
+        videoPlayer.clip = videoClips[randomClip];
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (videoPlayer.isPlaying == false)
+        {
+            Destroy(this.gameObject);
+        }
+
+        }
+}
